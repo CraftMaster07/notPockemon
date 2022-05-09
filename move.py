91da10ea2c7653ed22ee4557b445dbc431ff39be
CraftMaster07 <99757789+CraftMaster07@users.noreply.github.com>
2022-05-09 14:49:11 +0300

    Create move.py
    created the file
    imported some moves into it

---
+++ move.py
@@ -0,0 +1,26 @@
+# **this is a sub file**
+# 
+# this file contains the classes of
+# the moves for the main game
+
+class moveAttackFormat():
+        # returns a list of attack details:
+        # (category, type, power, accuracy, times)
+        def __init__(self, category, tipe, power, accuracy, times):
+          self.category = category
+          self.tipe = tipe
+          self.power = power
+          self.accuracy = accuracy
+          self.times = times
+          
+class basicAttack(moveAttackFormat):
+  def __init__(self):
+    super().__init__(0, 0, 40, 100, 1)
+
+class multyHitAttack(moveAttackFormat):
+  def __init__(self):
+    super().__init__(0, 0, 15, 85, 5)
+    
+class basicAtkIncrease():
+  def __init__(self, fighter):
+    fighter.Atk += fighter.baseAtk * 0.5
