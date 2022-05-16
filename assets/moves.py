926d5dca1ba17a301a360dab8e0206dfb465aa7e
CraftMaster07 <99757789+CraftMaster07@users.noreply.github.com>
2022-05-16 15:13:00 +0300

    Update moves.py
    added moves

--- assets/moves.py
+++ assets/moves.py
@@ -12,21 +12,55 @@ class moveAttackFormat():
           self.power = power
           self.accuracy = accuracy
           self.times = times
-          
-class basicAttack(moveAttackFormat):
+
+
+class tackle(moveAttackFormat):
   def __init__(self):
-    super().__init__(0, 0, 40, 100, 1)
+    super().__init__("normal", 0, 40, 100, 1)
+
 
 class quickAttack(moveAttackFormat):
   def __init__(self):
-    super().__init__(0, 0, 40, 100, 1)
+    super().__init__("normal", 0, 40, 100, 1)
     self.priority = 1
 
 
-class multyHitAttack(moveAttackFormat):
+class doubleSlap(moveAttackFormat):
   def __init__(self):
-    super().__init__(0, 0, 15, 85, 5)
-    
+    super().__init__("normal", 0, 15, 85, 5)
+
+
+class strength(moveAttackFormat):
+    def __init__(self):
+        super().__init__("normal", 0, 80, 100, 1)
+
+
+class xScissor(moveAttackFormat):
+    def __init__(self):
+        super().__init__("bug", 0, 80, 100, 1)
+
+
+class airSlash(moveAttackFormat): # not gen1
+  def __init__(self):
+    super().__init__("flying", 0, 75, 95, 1)
+
+
+class wingAttack(moveAttackFormat):
+  def __init__(self):
+    super().__init__("flying", 0, 35, 100, 1)
+
+
+# stat moves should have type somehow 
 class basicAtkIncrease():
   def __init__(self, fighter):
-    fighter.Atk += fighter.baseAtk * 0.5
+    fighter.atk += fighter.baseAtk * 0.5
+
+
+class tailWind(): # should last 3 turns
+    def __init__(self, fighter):
+        fighter.spd += fighter.baseSpd
+
+class tailWhip():
+   def __init__(self, enemy):
+        enemy.def -= enemy.baseDef * 0.5
+
